b332c815b9178e4e9eeb7b5e20e97aa7b81efd94
Sarah Payne <sarah@Sarahs-MacBook-Pro.local>
2017-09-22 13:43:01 -0500

    create server.js, update file structure

---
+++ models/Article.js
@@ -0,0 +1,26 @@
+// Article model
+
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var articleSchema = new Schema ({
+    title : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    url: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    date: {
+        type: String,
+        required: true
+    }
+})
+
+var Article = mongoose.model("Article", articleSchema);
+
+module.exports = Article;
---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require("express"),
+    mongoose = require("mongoose"),
+    expressHandlebars = require("express-handlebars"),
+    bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+var router = express.Router();
+
+require("./config/routes")(router);
+
+app.use(express.static(__dirname + "/public"));
+
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(router);
+
+var db = process.env.MONGODBURI || "mongodb://localhost/nytreact";
+
+mongoose.connect(db, function (error) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log("mongoose connection is successful");
+    }
+});
+
+app.listen(PORT, function() {
+    console.log("Listening on port:" + PORT);
+});
